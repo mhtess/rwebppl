50a4db429c658172370df14b5512f8a75d78ed8f
Mika Braginsky <mikabr@stanford.edu>
2016-03-04 14:32:47 -0800

    webppl script splits output into lines, more correct code org

--- R/rwebppl.R
+++ R/rwebppl.R
@@ -1,9 +1,27 @@
-model_code <- "
-  [flip(0.5), flip(0.3)]
-"
+tidy_output <- function(model_output) {
+  if (!is.null(names(model_output)) &&
+      all(names(model_output) == c("probs", "support"))) {
+    data.frame(support = model_output$support,
+               probs = model_output$probs)
+  } else {
+    model_output
+  }
+}
 
+#' webppl
+#'
+#' Runs webppl model.
+#'
+#' @param model_code A string of a webppl program.
+#' @param model_file A file containing a webppl program.
+#'
+#' @return The model's return value(s).
+#' @export
+#'
+#' @examples
+#' model_code <- "flip(0.5)"
+#' webppl(model_code)
 webppl <- function(model_code = NULL, model_file = NULL) {
-  #assertthat::validate_that((!is.null(model_file) && file.exists(model_file)) | !is.null(model_code))
   if (!is.null(model_code)) {
     cat(model_code, file = (f <- tempfile()))
   } else if (!is.null(model_file) && file.exists(model_file)) {
@@ -11,12 +29,9 @@ webppl <- function(model_code = NULL, model_file = NULL) {
   } else {
     stop("no model file or model code supplied")
   }
-  output_string <- system2("./exec/rwebppl", args = c(f), stdout = TRUE)
-  output <- jsonlite::fromJSON(output_string)
-  if (all(names(output) == c("probs", "support"))) {
-    data.frame(support = output$support,
-               probs = output$probs)
-  } else {
-    output
-  }
+  js_path <- system.file("js", package = "rwebppl")
+  script_path <- file.path(js_path, "rwebppl")
+  output_string <- paste(system2(script_path, args = c(f), stdout = TRUE),
+                         collapse = "")
+  tidy_output(jsonlite::fromJSON(output_string))
 }
