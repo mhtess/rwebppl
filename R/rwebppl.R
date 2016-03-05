42176bf0031238e3f650bdbad86cae490ec73a07
Mika Braginsky <mikabr@stanford.edu>
2016-03-04 19:41:15 -0800

    onLoad hook to install webppl

--- R/rwebppl.R
+++ R/rwebppl.R
@@ -1,13 +1,27 @@
-#' Title
+#' Install webppl
 #'
 #' @return
 #' @export
 #'
 #' @examples
+#' \dontrun{install_webppl()}
 install_webppl <- function() {
   pkg_path <- system.file(package = "rwebppl")
-  system(sprintf("cd %s; npm init -y . &>/dev/null; npm install --save webppl &>/dev/null; cd node_modules/webppl; npm install &>/dev/null", pkg_path),
+  system(sprintf("cd %s && \
+                 npm init -y . &>/dev/null && \
+                 npm install --save webppl &>/dev/null &&\
+                 cd node_modules/webppl &&\
+                 npm install &>/dev/null",
+                 pkg_path),
          ignore.stdout = TRUE, ignore.stderr = TRUE)
+}
+
+.onLoad <- function(libname, pkgname) {
+  pkg_path <- system.file(package = "rwebppl")
+  if (!file.exists(file.path(pkg_path, "package.json"))) {
+    cat("webppl not found, installing...")
+    install_webppl()
+  }
 }
 
 tidy_output <- function(model_output) {
@@ -26,6 +40,7 @@ tidy_output <- function(model_output) {
 #'
 #' @param model_code A string of a webppl program.
 #' @param model_file A file containing a webppl program.
+#' @param model_packages A character vector of external package names to use.
 #'
 #' @return The model's return value(s).
 #' @export
@@ -33,7 +48,8 @@ tidy_output <- function(model_output) {
 #' @examples
 #' model_code <- "flip(0.5)"
 #' webppl(model_code)
-webppl <- function(model_code = NULL, model_file = NULL, model_packages = NULL) {
+webppl <- function(model_code = NULL, model_file = NULL,
+                   model_packages = NULL) {
   if (!is.null(model_code)) {
     cat(model_code, file = (f <- tempfile()))
   } else if (!is.null(model_file) && file.exists(model_file)) {
@@ -42,13 +58,15 @@ webppl <- function(model_code = NULL, model_file = NULL, model_packages = NULL) 
     stop("no model file or model code supplied")
   }
   if (!is.null(model_packages)) {
-    package_args <- paste(lapply(model_packages, function(x) paste("--require", x)),
-                          collapse = " ")
+    package_args <- unlist(lapply(model_packages,
+                                  function(x) paste("--require", x)))
   } else {
     package_args <- ""
   }
   script_path <- system.file("js/rwebppl", package = "rwebppl")
-  output_string <- paste(system2(script_path, args = c(f, package_args), stdout = TRUE),
-                         collapse = "")
+  output_string <- paste(
+    system2(script_path, args = c(f, package_args), stdout = TRUE),
+    collapse = ""
+  )
   tidy_output(jsonlite::fromJSON(output_string))
 }
