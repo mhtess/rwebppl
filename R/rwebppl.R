d5762ddba191a3b521a30225d207ff9cd9b2ed3a
Mika Braginsky <mikabr@stanford.edu>
2016-04-24 18:39:10 -0700

    rwebppl js writes temporary output then moves file #6, rwebppl function is commented

--- R/rwebppl.R
+++ R/rwebppl.R
@@ -35,7 +35,8 @@ tidy_output <- function(model_output) {
 #'
 #' @param model_code A string of a webppl program.
 #' @param model_file A file containing a webppl program.
-#' @param data A data frame (or other serializable object) that can be referenced in the model.
+#' @param data A data frame (or other serializable object) that can be
+#'   referenced in the model.
 #' @param data_var A name by which data can be referenced in the model.
 #' @param model_packages A character vector of external package names to use.
 #'
@@ -47,36 +48,62 @@ tidy_output <- function(model_output) {
 #' webppl(model_code)
 webppl <- function(model_code = NULL, model_file = NULL, data = NULL,
                    data_var = NULL, model_packages = NULL) {
+
+  # find location of rwebppl JS script, within rwebppl R package
+  script_path <- system.file("js/rwebppl", package = "rwebppl")
   packages <- model_packages
-  if (!is.null(data) && !is.null(data_var)) {
-    tmp_dir <- tempdir()
-    dir.create(file.path(tmp_dir, data_var), showWarnings = FALSE)
-    cat(sprintf('{"name":"%s","main":"index.js"}', data_var),
-        file = file.path(tmp_dir, data_var, "package.json"))
-    data_string <- jsonlite::toJSON(data)
-    cat(sprintf("module.exports = JSON.parse('%s')", data_string),
-        file = file.path(tmp_dir, data_var, "index.js"))
-    packages <- c(packages, file.path(tmp_dir, data_var))
+
+  # if data supplied, create a webppl package that exports the data as data_var
+  if (!is.null(data)) {
+    if (is.null(data_var)) {
+      warning("ignoring data (supplied without data_var)")
+    } else {
+      tmp_dir <- tempdir()
+      dir.create(file.path(tmp_dir, data_var), showWarnings = FALSE)
+      cat(sprintf('{"name":"%s","main":"index.js"}', data_var),
+          file = file.path(tmp_dir, data_var, "package.json"))
+      data_string <- jsonlite::toJSON(data)
+      cat(sprintf("module.exports = JSON.parse('%s')", data_string),
+          file = file.path(tmp_dir, data_var, "index.js"))
+      packages <- c(packages, file.path(tmp_dir, data_var))
+    }
   }
+
+  # set file_arg to temporary file containing model_code or to mode_file
   if (!is.null(model_code)) {
-    cat(model_code, file = (f <- tempfile()))
-  } else if (!is.null(model_file) && file.exists(model_file)) {
-    f <- model_file
+    if (!is.null(model_file)) {
+      warning("both model_code and model_file supplied, using model_code")
+    }
+    cat(model_code, file = (file_arg <- tempfile()))
+  } else if (!is.null(model_file)) {
+    if (!file.exists(model_file)) {
+      stop("model_file does not exist")
+    }
+    file_arg <- model_file
   } else {
-    stop("no model file or model code supplied")
+    stop("supply one of model_code or model_file")
   }
+
+  # create --require argument out of each package name
   if (!is.null(packages)) {
     package_args <- unlist(lapply(packages,
                                   function(x) paste("--require", x)))
   } else {
     package_args <- ""
   }
-  script_path <- system.file("js/rwebppl", package = "rwebppl")
+
+  # clear path where rwebppl JS script will write output
   output_file = "/tmp/webppl_output"
   if (file.exists(output_file)) {
     file.remove(output_file)
   }
-  system2(script_path, args = c(f, package_args), stdout = "", wait = FALSE)
+
+  # run rwebppl JS script with model file and packages as arguments
+  # any output to stdout gets sent to the R console while command runs
+  system2(script_path, args = c(file_arg, package_args),
+          stdout = "", wait = FALSE)
+
+  # wait for output file to exist, then collect and tidy results
   while (!file.exists(output_file)) {Sys.sleep(1)}
   output_string <- paste(readLines(output_file), collapse = "\n")
   if (output_string != "") {
