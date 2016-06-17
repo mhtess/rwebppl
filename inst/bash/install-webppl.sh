a182935a5a744013f8d762f9844b823e4c270bca
hawkrobe <rxdh@stanford.edu>
2016-06-16 17:22:16 -0700

    Install locally

--- inst/bash/install-webppl.sh
+++ inst/bash/install-webppl.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-npm install -g webppl
+RWEBPPL=$1
+
+cd $RWEBPPL
+cp json/webppl-packages.json js/package.json
+
+cd js
+npm install webppl
+
+# Make this look as though it were symlinked, so we can use local paths
+mv node_modules/webppl/ .
+mv node_modules webppl/node_modules
