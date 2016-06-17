66f36e0850322ea85c388fb732eaffeafe9253a4
hawkrobe <rxdh@stanford.edu>
2016-06-16 18:34:30 -0700

    Install webppl as dependency in package.json

--- inst/bash/install-webppl.sh
+++ inst/bash/install-webppl.sh
@@ -3,10 +3,10 @@
 RWEBPPL=$1
 
 cd $RWEBPPL
-cp json/webppl-packages.json js/package.json
+cp json/rwebppl.json js/package.json
 
 cd js
-npm install webppl
+npm install
 
 # Make this look as though it were symlinked, so we can use local paths
 mv node_modules/webppl/ .
